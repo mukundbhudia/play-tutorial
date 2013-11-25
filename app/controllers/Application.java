fb41b37aaf43249ca24be2c51312c065da253214
Mukund Bhudia <mukund_bhudia@hotmail.com>
2013-11-25 10:11:34 +0000

    Added routes for task operations prepared controller for new routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,8 +10,19 @@ import java.util.Date;
 public class Application extends Controller {
 
     public static Result index() {
-        Date today = new Date();
-        return ok("Hello world!\r\nThe time is: " + today.toString());
+        return redirect(routes.Application.tasks());
+    }
+
+    public static Result tasks(){
+        return TODO;
+    }
+
+    public static Result newTask(){
+        return TODO;
+    }
+
+    public static Result deleteTask(Long id){
+        return TODO;
     }
 
 }
