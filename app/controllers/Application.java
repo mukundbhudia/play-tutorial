d8ff4cbbe9ce86d9f5a45b1f20d859452c8f35f3
Mukund Bhudia <mukund_bhudia@hotmail.com>
2013-11-26 10:37:25 +0000

    Wired up task display in controller. Made task a required feild.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import models.Task;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -9,12 +11,16 @@ import java.util.Date;
 
 public class Application extends Controller {
 
+    static Form<Task> taskForm = Form.form(Task.class);
+
     public static Result index() {
-        return redirect(routes.Application.tasks());
+        return redirect(controllers.routes.Application.tasks());
     }
 
     public static Result tasks(){
-        return TODO;
+        return ok(
+            views.html.index.render(Task.all(), taskForm)
+        );
     }
 
     public static Result newTask(){
--- app/models/Task.java
+++ app/models/Task.java
@@ -1,5 +1,7 @@
 package models;
 
+import play.data.validation.Constraints;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,8 @@ import java.util.List;
 public class Task {
 
     public Long id;
+
+    @Constraints.Required
     public String label;
 
     public static List<Task> all() {
