6267cd6992e6e40658544a2fb88eae5795874c5f
Mukund Bhudia <mukund_bhudia@hotmail.com>
2013-11-27 10:19:51 +0000

    Amended Task class for persistence to DB. Added configurations for H" db.

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,30 +1,38 @@
 package models;
 
-import play.data.validation.Constraints;
+import play.data.validation.*;
+import play.db.ebean.*;
 
+import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
     @Author Mukund Bhudia
  */
-public class Task {
+@Entity
+public class Task extends Model {
 
+    @Id
     public Long id;
 
     @Constraints.Required
     public String label;
 
+    public static Finder<Long , Task> find = new Finder(
+            Long.class, Task.class
+    );
+
     public static List<Task> all() {
-        return new ArrayList<Task>();
+        return find.all();
     }
 
     public static void create(Task task) {
-
+        task.save();
     }
 
     public static void delete(Long id) {
-
+        find.ref(id).delete();
     }
 
 }
